8580c577ec2a3d2665624e73f30b517d8e3d0308
Mr. Mac-Flo (The Assist) <70183048+maciam21@users.noreply.github.com>
2023-04-10 17:02:07 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
     var magic8Ball = {};
-    magic8Ball.listofanswers = ["Yes, 👴🏻 My Guy", "💅🏼Si Ms. Girl! 💁🏽‍♀️","Hell No Fresno 🚫","🤬Ph*@%k No 🖕🏼", "Tu que crees 🤨", "Waki 🤢","Ch!*g@ Tu M@dr* 🤬","Heck yes, Fres Yes! 👌", "Good Answer Steve!", "Bruhh..🙄" ,"Bye, Felicia! ✌🏼", "Ms. Girl..💅🏼 Please","Duh Felipe!🤦🏻‍♀️", "Te apesta a culo 🍑","Hasta La Vista Baby", "You're not the father 👨", "No for I have Spoken 🚫", "Si Perro 🐶", "Ask my boii Dennis", "Ms. Girl 💅🏼", "👸🏻Material Girl 💅🏼","No M@m*s Way 🍆","La Traes Verde 💩","🥋Catch these hands 🥊","Yo moms a h*# 💁🏽‍♀️", "Yaaass 👸🏻 Queen ","No Pues Wow 😒","Ask Future 🤖", "Tus N@lg@$ 🍑 dipped in Vinagre", "Really h*#? 😒","Tu mama wey 🤰🏻","🤬 C%*h!ng@ 20 🖕🏼"];
+    magic8Ball.listofanswers = ["Yes, 👴🏻 My Guy", "💅🏼Si Ms. Girl! 💁🏽‍♀️","Hell No Fresno 🚫","🤬Ph*@%k No 🖕🏼", "Tu que crees 🤨", "Waki 🤢","Ch!*g@ Tu M@dr* 🤬","Heck yes, Fres Yes! 👌", "Good Answer Steve!🧑🏻 ", "Bruhh..🙄" ,"Bye, Felicia! ✌🏼", "Ms. Girl..💅🏼 Please","Duh Felipe!🤦🏻‍♀️", "Te apesta a culo 🍑💨","Hasta La Vista Baby😎 ", "🚫 You're not the father 👨", "No for I have Spoken 🚫", "Si Perro 🐶", "Ask my boii Dennis  👨", "Ms. Girl 💅🏼", "👸🏻Material Girl 💅🏼","No M@m*s Way 🍆","La Traes Verde 💩","🥋Catch these hands 🥊","Yo moms a h*# 💁🏽‍♀️", "Yaaass 👸🏻 Queen ","No Pues Wow 😒","Ask Future 🤖", "Tus N@lg@$ 🍑 dipped in Vinagre", "Really h*#? 😒","Tu m@m@ Way 🤰🏻","🤬 C%*h!ng@ 20 🖕🏼"];
    
     magic8Ball.getAnswer = function(question)
     {
